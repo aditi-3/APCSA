e96d5175d5e7c952eac93405cee2f64dea4d00ca
aditi-3 <75766710+aditi-3@users.noreply.github.com>
2021-03-18 10:53:50 -0700

    Add files via upload

---
+++ Cereal.java
@@ -0,0 +1,38 @@
+public class Cereal
+{
+     private int potassium;
+    private String name = "";
+
+    public Cereal(String name, int potassium)
+    {
+        this.potassium = potassium;
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPotassium()
+    {
+        return potassium;
+    }
+
+    public String toString()
+    {
+        return name; 
+    }
+
+    public static void main(String[] args)
+    {
+        Cereal cocoaPuffs = new Cereal("Cocoa Puffs", 55);
+        Cereal allBranFiber = new Cereal("All-Bran with Extra Fiber", 330);
+        Cereal branChex = new Cereal("Bran Chex", 190);
+
+        System.out.println(cocoaPuffs.getName());
+        System.out.println(cocoaPuffs.getPotassium());
+        System.out.println(cocoaPuffs);
+
+    }
+}
